709b9e2e6056cd88225b853b489b864790856a15
HatsiL-64 <324082625@alumno.enp.unam.mx>
2025-09-21 22:11:57 -0600

    Arregle un segundo problema en el bat. Las rutas

--- Palabras.bat
+++ Palabras.bat
@@ -3,7 +3,7 @@ IF NOT EXIST programa.exe (
 	cd src
 	attrib +R * 
 	attrib +H .palabras.txt
-	cd ..\biblioteca
+	cd ..\.biblioteca
 	gcc -c funjs.c -o funjs.o
 	cd ..
 	gcc programa.c .biblioteca\funjs.o -o programa.exe -lallegro -lallegro_primitives -lallegro_image -lallegro_font -lallegro_ttf
