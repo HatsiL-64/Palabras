bb2696ed0d310cdea01b33ed7cbae8bd0d2d0829
HatsiL-64 <324082625@alumno.enp.unam.mx>
2025-09-21 21:54:40 -0600

    Agregue mas palabras y solucione un problema en el archivo bat

--- Palabras.bat
+++ Palabras.bat
@@ -1,12 +1,13 @@
+@echo off
 IF NOT EXIST programa.exe (
 	cd src
 	attrib +R * 
 	attrib +H .palabras.txt
-	cd ../biblioteca
+	cd ..\biblioteca
 	gcc -c funjs.c -o funjs.o
 	cd ..
-	gcc programa.c .biblioteca/funjs.o -o programa.exe -lallegro -lallegro_primitives -lallegro_image -lallegro_font -lallegro_ttf
-	rd /Q /S .biblioteca
+	gcc programa.c .biblioteca\funjs.o -o programa.exe -lallegro -lallegro_primitives -lallegro_image -lallegro_font -lallegro_ttf
+	REM rd /Q /S .biblioteca
 	del programa.c
 )
 IF EXIST programa.exe ./programa.exe
